0fd1b52dbfd400a5f4aa5319b1b802e20a9b5570
valvijo <valeriovjoao@gmail.com>
2019-04-10 17:50:57 +0200

    Create transformation for tbStatus

--- sql/iSante-plus.sql
+++ sql/iSante-plus.sql
@@ -1,6 +1,5 @@
---we need to handle dupplication on replicate-initial-data-transformation
---alter table patient add last_repl_timestamp datetime null;
---insert into patient_identifier_type (name, description, creator, date_created, uuid, uniqueness_behavior) values ('iSante patient.person_id', 'Value of patient.person_id from iSante', 1, CURDATE(), '96194f0a-65c6-45b4-b647-feaa6050a52c', 'UNIQUE');
+#we need to handle dupplication on replicate-initial-data-transformation alter table patient add last_repl_timestamp datetime null;
+#insert into patient_identifier_type (name, description, creator, date_created, uuid, uniqueness_behavior) values ('iSante patient.person_id', 'Value of patient.person_id from iSante', 1, CURDATE(), '96194f0a-65c6-45b4-b647-feaa6050a52c', 'UNIQUE');
 
 create table etl_execution (
   id bigint(20) not null auto_increment,
@@ -9,5 +8,5 @@ create table etl_execution (
   primary key (id)
 );
 
---to simplify processing logic lets pretend there was an initial execution in 1900
+#to simplify processing logic lets pretend there was an initial execution in 1900
 insert into etl_execution values (null, '1900-01-01', true);
