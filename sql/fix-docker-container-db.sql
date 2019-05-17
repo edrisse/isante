14a1c0045c1ecc57ff67eb29d762a7dc8d309df2
Edrisse <edrisseasm@gmail.com>
2019-05-17 10:36:53 +0200

    Fix isante_location_id validation, transformation to generate location

--- sql/fix-docker-container-db.sql
+++ sql/fix-docker-container-db.sql
@@ -2,4 +2,4 @@ insert into location_attribute
     (location_id, attribute_type_id, value_reference, uuid, creator, date_created, voided)
 values 
     (1, 1, '11111', '09fe3ea5-903b-486c-9f50-36f529489569', 1, curdate(), false);
---update person set voided = true where person.person_id in (284, 326, 329);
+--update obs set location_id = 1 where location_id is null
